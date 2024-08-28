3ddc3bc9cf09afd94da188dd2d861b6164081ca7
uyk <88850669+uYanki@users.noreply.github.com>
2024-08-29 01:12:29 +0800

    tiny

---
+++ components/tinyaes/aes_cfg_template.h
@@ -0,0 +1,12 @@
+#ifndef _AES_CFG_H_
+#define _AES_CFG_H_
+
+#define CBC    1  // AES encryption in CBC-mode of operation.
+#define ECB    1  // encryption in counter-mode.
+#define CTR    1  // the basic ECB 16-byte block algorithm. All can be enabled simultaneously.
+
+#define AES128 1
+// #define AES192 1
+// #define AES256 1
+
+#endif  // _AES_H_
